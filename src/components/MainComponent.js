45bea65305e05b497780af1d367e33bc2cf07453
Harpreet Singh <dhillonpbx@gmail.com>
2020-07-13 17:08:06 +0530

    Assignment 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,6 +3,7 @@ import { Navbar, NavbarBrand } from 'reactstrap';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 import DishDetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';
 import Header from './HeaderComponent';
@@ -51,6 +52,7 @@ class Main extends Component {
             <Route path='/home' component={HomePage} />
             <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
             <Route exact path='/contactus' component={Contact} />} />
+            <Route exact path='/aboutus' component={() => <About leaders={this.state.leaders} />} />
             <Route path='/menu/:dishId' component={DishWithId} />
             <Redirect to="/home" />
         </Switch>
