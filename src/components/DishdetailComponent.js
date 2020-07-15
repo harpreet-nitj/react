d317688a4e4906b79a1ec542b7dc9d3c2f988ebb
Harpreet Singh <dhillonpbx@gmail.com>
2020-07-15 13:15:22 +0530

    Fetch Post Comment.

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -24,7 +24,7 @@ function RenderDish({dish}) {
               </div>
           );
   }
- function RenderComments({comments, addComment, dishId}) {
+ function RenderComments({comments, postComment, dishId}) {
       if (comments != null)
           return(
               <div className="col-12 col-md-5 m-1">
@@ -39,7 +39,7 @@ function RenderDish({dish}) {
                       );
                   })}
                   </ul>
-                  <CommentForm dishId={dishId} addComment={addComment} />
+                  <CommentForm dishId={dishId} postComment={postComment} />
               </div>
           );
       else
@@ -69,7 +69,7 @@ function RenderDish({dish}) {
 
     handleSubmit(values) {
       this.toggleModal();
-      this.props.addComment(this.props.dishId, values.rating, values.author, values.message);
+      this.props.postComment(this.props.dishId, values.rating, values.author, values.message);
     }
 
     render(){
@@ -173,7 +173,7 @@ function RenderDish({dish}) {
           <div className="row">
               <RenderDish dish={props.dish} />
               <RenderComments comments={props.comments}
-              addComment={props.addComment}
+              postComment={props.postComment}
               dishId={props.dish.id} />
           </div>
         </div>
